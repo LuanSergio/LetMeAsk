1d29e23f31ee08fe51be7043cc3bd27692223cd7
Luan <luansergiomattos@gmail.com>
2021-06-30 23:55:34 -0300

    refactor: redirect user to admin after room is created

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -29,8 +29,8 @@ export function NewRoom() {
       authorId: user?.id,
     });
 
-    history.push(`/rooms/${firebaseRoom.key}`);
-    // history.push(`/admin/rooms/${firebaseRoom.key}`);
+    // history.push(`/rooms/${firebaseRoom.key}`);
+    history.push(`/admin/rooms/${firebaseRoom.key}`);
   }
 
   return (
